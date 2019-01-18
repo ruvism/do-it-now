313b93c5e596ec5bd2560d65cc0703c5cea7f436
ruvila <ruvila.m@gmail.com>
2019-01-18 05:04:36 -0200

    tentativa criacao tarefas

--- scripts/listas.js
+++ scripts/listas.js
@@ -1,34 +1,21 @@
 // criacao de tarefas
 
-// function criarTarefa(){
+function criarTarefa(){
+        const botao = document.getElementById("botao__mais");
+        botao.addEventListener("click", function(){
+            const linhaTarefa = document.createElement("nova__linha__tarefa");
+            const afazer = document.getElementById("tarefa__adicionar").value;
+            const novaTarefa = document.createTextNode(afazer);
+            linhaTarefa.appendChild(novaTarefa);
+    document.getElementById("criacao__tarefa__lista").appendChild(linhaTarefa);
+    document.getElementById("tarefa__adicionar").value = "";
+})
+}
 
-//     var novaTarefa = document.getElementById("tarefa__adicionar").value;
-
-//     var conteudoAtual = document.getElementById("criacao__tarefa__lista").innerHTML;
-
-//     var novaLinha = '<li onclick="concluirTarefa(this)">' + novaTarefa + '<span onclick="deletarTarefa(this)"><img src="./assets/icone_deletar_tarefa-subtarefa.png"></span>' + '</li>';	
-//     output.innerHTML = conteudoAtual + novaLinha;
-
-//     document.getElementById("tarefa__adicionar").value = '';
-// }
-			
 function concluirTarefa(feita) {
     feita.classList.toggle('checked');       
 }
 
 function deletarTarefa(botaoFechar){
     botaoFechar.parentElement.remove()
-}
-
-
-function criarTarefa(){
-    const linhaTarefa = document.createElement("nova__linha__tarefa");
-    const afazer = document.getElementById("tarefa__adicionar").value;
-    const novaTarefa = document.createTextNode(afazer);
-    if( afazer == ""){
-        alert("Coloque sua tarefa aqui")
-    } else{
-        document.getElementById("criacao__tarefa__lista").appendChild(linhaTarefa);
-    }
-    document.getElementById("tarefa__adicionar").value = "";
 }
